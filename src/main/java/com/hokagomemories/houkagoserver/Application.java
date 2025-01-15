972a663690a886eab1270a1dc379f2ea7b40fa4e
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-16 03:32:06 +0900

    Initial commit

---
+++ src/main/java/com/hokagomemories/houkagoserver/Application.java
@@ -0,0 +1,13 @@
+package com.hokagomemories.houkagoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
