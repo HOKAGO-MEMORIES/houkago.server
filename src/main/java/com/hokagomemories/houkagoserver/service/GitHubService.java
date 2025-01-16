9a123c0a4d0113168a3ed76a3c2dc7d78c4a1f63
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-16 23:59:13 +0900

    feat: GitHub API 호출 후 파일 내용 가져오는 서비스 구현

---
+++ src/main/java/com/hokagomemories/houkagoserver/service/GitHubService.java
@@ -0,0 +1,38 @@
+package com.hokagomemories.houkagoserver.service;
+
+import com.hokagomemories.houkagoserver.repository.GitHubApiConfig;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Service
+public class GitHubService {
+
+    private final GitHubApiConfig gitHubApiConfig;
+    private final RestTemplate restTemplate;
+
+    public GitHubService() {
+        this.gitHubApiConfig = new GitHubApiConfig();
+        this.restTemplate = new RestTemplate();
+    }
+
+    public String getFileContent(String path) throws IOException {
+        String apiUrl = gitHubApiConfig.getGithubApiUrl() + "/contents/" + path;
+        String token = gitHubApiConfig.getGithubToken();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "token " + token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, entity, String.class);
+
+        if (response.getStatusCode() == HttpStatus.NOT_FOUND) {
+            throw new FileNotFoundException("파일이 없습니다.");
+        }
+
+        return response.getBody();
+    }
+}
