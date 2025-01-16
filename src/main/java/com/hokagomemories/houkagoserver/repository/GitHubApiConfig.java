7d4a0b89eb90c6ac931d44071ec39d9d8ef2decd
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-16 22:52:25 +0900

    feat: 깃허브 api 불러오기 구현

---
+++ src/main/java/com/hokagomemories/houkagoserver/repository/GitHubApiConfig.java
@@ -0,0 +1,34 @@
+package com.hokagomemories.houkagoserver.repository;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.Getter;
+
+@Getter
+public class GitHubApiConfig {
+
+    private static final Dotenv dotenv = Dotenv.load();
+    private final String githubApiUrl;
+    private final String githubToken;
+
+    public GitHubApiConfig() {
+        githubApiUrl = validateEnv(loadGithubApiUrl());
+        githubToken = validateEnv(loadGithubToken());
+    }
+
+    private String loadGithubApiUrl() {
+        return dotenv.get("GITHUB_API_URL");
+    }
+
+    private String loadGithubToken() {
+        return dotenv.get("GITHUB_TOKEN");
+    }
+
+    private String validateEnv(String env) {
+        if (env == null) {
+            throw new IllegalArgumentException("GitHub API URL or Token is missing in the .env file");
+        }
+        return env;
+    }
+}
+
+
