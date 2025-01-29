5d9600051693a865f45c1b0a0863a47a1cac419f
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-29 23:18:46 +0900

    feat: permalink 추가

--- src/main/java/com/hokagomemories/houkagoserver/dto/PostMetadata.java
+++ src/main/java/com/hokagomemories/houkagoserver/dto/PostMetadata.java
@@ -9,8 +9,9 @@ public class PostMetadata {
     private String title;
     private String date;
     private String desc;
-    private String from;
+    private String category;
     private String slug;
     private String thumbnail;
     private String content;
+    private String permalink;
 }
