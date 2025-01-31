8a2630c37eb6cd85422e7f3bc5469ba2bb656809
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-01-31 21:16:05 +0900

    feat: 포스트를 불러오는데 필요한 파일 생성 기능 추가

--- src/main/java/com/hokagomemories/houkagoserver/service/JsonGenerationService.java
+++ src/main/java/com/hokagomemories/houkagoserver/service/JsonGenerationService.java
@@ -1,47 +1,82 @@
 package com.hokagomemories.houkagoserver.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hokagomemories.houkagoserver.config.GitHubApiConfig;
 import com.hokagomemories.houkagoserver.dto.PostMetadata;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JsonGenerationService {
-    private static final String OUTPUT_DIR = ".posts";
-    private static final String BASE_URL = "http://localhost:8080/api/posts";
     private final ObjectMapper objectMapper;
+    private final GitHubApiConfig gitHubApiConfig = new GitHubApiConfig();
+
+    private static final String OUTPUT_DIR = "/.posts";
+    private final String BASE_URL = gitHubApiConfig.getGithubImageUrl();
 
     public JsonGenerationService() {
         this.objectMapper = new ObjectMapper();
-        createOutputDirectory();
     }
 
-    private void createOutputDirectory() {
-        File directory = new File(OUTPUT_DIR);
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
+    public void generateFiles(String blogRoot, List<PostMetadata> blogPosts, List<PostMetadata> psPosts) throws IOException {
+        String outputDir = blogRoot + OUTPUT_DIR;
+        createDirectories(outputDir);
+        System.out.println("Directory created at: " + outputDir);
+
+        File blogFile = new File(outputDir + "/blogPosts.json");
+        File psFile = new File(outputDir + "/psPosts.json");
+
+        objectMapper.writeValue(blogFile, transformPosts(blogPosts, "blog"));
+        objectMapper.writeValue(psFile, transformPosts(psPosts, "ps"));
+
+        System.out.println("Files generated at: " + blogFile.getAbsolutePath());
+        System.out.println("Files generated at: " + psFile.getAbsolutePath());
+
+        generateIndexTs(outputDir);
+        generateIndexDts(outputDir);
     }
 
-    public void generateJson(List<PostMetadata> blogPosts, List<PostMetadata> psPosts) throws IOException {
-        List<PostMetadata> transformedBlogPosts = transformPosts(blogPosts, "blog");
-        List<PostMetadata> transformedPsPosts = transformPosts(psPosts, "ps");
-
-        objectMapper.writeValue(new File(OUTPUT_DIR + "/blogPosts.json"), transformedBlogPosts);
-        objectMapper.writeValue(new File(OUTPUT_DIR + "/psPosts.json"), transformedPsPosts);
+    private void createDirectories(String outputDir) throws IOException {
+        Files.createDirectories(Path.of(outputDir));
     }
+
+    private void generateIndexTs(String outputDir) throws IOException {
+        String content = """
+                export { default as blogPosts } from './blogPosts.json'
+                export { default as psPosts } from './psPosts.json'
+                """;
+
+        Files.writeString(Path.of(outputDir + "/index.ts"), content);
+    }
+
+    private void generateIndexDts(String outputDir) throws IOException {
+        String content = """
+                import type { Post } from '../src/types/post.ts'
+                
+                export type Blog = Post & { body: string }
+                export declare const blogPosts: Blog[]
+                
+                export type PS = Post & { body: string }
+                export declare const psPosts: PS[]
+                """;
+
+        Files.writeString(Path.of(outputDir + "/index.d.ts"), content);
+    }
+
 
     private List<PostMetadata> transformPosts(List<PostMetadata> posts, String category) {
         return posts.stream()
                 .map(post -> {
-                    String content = transformMarkdownImagePaths(post.getContent(), category, post.getSlug());
+                    String content = transformMarkdownImagePaths(post.getBody(), category, post.getSlug());
 
                     String permalink = "/" + post.getSlug().replace(category + "/", "");
 
-                    post.setContent(content);
+                    post.setBody(content);
                     post.setPermalink(permalink);
 
                     if (post.getThumbnail() != null) {
