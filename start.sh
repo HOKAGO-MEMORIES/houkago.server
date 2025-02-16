b4633bd130f76414f7f86ae796522c975427c67e
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-02-16 21:36:46 +0900

    feat: 배포 방식을 S3에서 ECR로 변경

--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+source /home/ubuntu/app/.env
+
+AWS_REGION="ap-northeast-2"
+ECR_REPO_URL="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/houkago-server"
+
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REPO_URL}
+
 docker stop houkago-server || true
 docker rm houkago-server || true
 
-source /home/ubuntu/app/.env
-
-docker build -t houkago-server /home/ubuntu/app
+docker pull ${ECR_REPO_URL}:latest
 
 docker run -d -p 80:80 \
-  -e GITHUB_API_URL=$GITHUB_API_URL \
-  -e GITHUB_IMAGE_URL=$GITHUB_IMAGE_URL \
-  -e GITHUB_TOKEN=$GITHUB_TOKEN \
-  -e ALLOWED_ORIGINS=$ALLOWED_ORIGINS \
-  -e DATABASE_URL=$DATABASE_URL \
-  -e DATABASE_USERNAME=$DATABASE_USERNAME \
-  -e DATABASE_PASSWORD=$DATABASE_PASSWORD \
-  --name houkago-server houkago-server
+  -e GITHUB_API_URL=${GITHUB_API_URL} \
+  -e GITHUB_IMAGE_URL=${GITHUB_IMAGE_URL} \
+  -e GITHUB_TOKEN=${GITHUB_TOKEN} \
+  -e ALLOWED_ORIGINS=${ALLOWED_ORIGINS} \
+  -e DATABASE_URL=${DATABASE_URL} \
+  -e DATABASE_USERNAME=${DATABASE_USERNAME} \
+  -e DATABASE_PASSWORD=${DATABASE_PASSWORD} \
+  --restart unless-stopped \
+  --name houkago-server ${ECR_REPO_URL}:latest
