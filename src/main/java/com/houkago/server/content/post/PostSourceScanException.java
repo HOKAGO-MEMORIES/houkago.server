efd411a06c09649e42b6b2f59f9dd6a7aa09778c
HOKAGO-MEMORIES <minchotejava@gmail.com>
2026-07-03 17:57:24 +0900

    feat: add local post source scanner

---
+++ src/main/java/com/houkago/server/content/post/PostSourceScanException.java
@@ -0,0 +1,8 @@
+package com.houkago.server.content.post;
+
+public class PostSourceScanException extends RuntimeException {
+
+	public PostSourceScanException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
